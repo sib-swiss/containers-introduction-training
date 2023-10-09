ce2b04bac18906a1ec0ecca5d85bd9cd61de0e4f
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2023-10-09 16:51:46 +0200

    updates instructions to connect

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -6,3 +6,15 @@ sudo add-apt-repository -y ppa:apptainer/ppa
 sudo apt update
 sudo apt install -y apptainer
 
+# install conda in /opt/miniconda3
+
+# install mamba as root:
+sudo su - 
+conda install -n base -c conda-forge mamba
+
+# install snake_course environment
+mamba env create -f conda/snakemake_course.yaml
+
+# install snakemake
+conda activate snake_course
+mamba install -c conda-forge -c bioconda snakemake
